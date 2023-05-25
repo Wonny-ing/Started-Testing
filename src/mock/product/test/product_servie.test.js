a0eeb662bd24cb8b96f006d8d19f6236f9a4dc74
Wonny-ing <h.wonny11@gmail.com>
2023-05-25 23:34:30 +0900

    user test

--- src/mock/product/test/product_servie.test.js
+++ src/mock/product/test/product_servie.test.js
@@ -8,8 +8,14 @@ describe('ProductService - Stub', () => {
     productService = new ProductService(new StubPRoductClient());
   });
 
-  it('should filter out only awailable items', async () => {
+  it('should filter out only available items', async () => {
+    // Arrange, Given - 준비
+    // productService = new ProductService(new StubPRoductClient());
+
+    // Act, When - 실헹
     const items = await productService.fetchAvailableItems();
+
+    // Assert, Then - 단언
     expect(items.length).toBe(1);
     expect(items).toEqual([{ item: '🥛', available: true }]);
   });
